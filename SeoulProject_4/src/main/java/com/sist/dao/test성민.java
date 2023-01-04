bbff4095b5307e60da871bf8197b799b270a59c0
josungmin <josungmin@192.168.0.2>
2023-01-04 11:23:23 +0900

    test

--- SeoulProject_4/src/main/java/com/sist/dao/test성민.java
+++ SeoulProject_4/src/main/java/com/sist/dao/test성민.java
@@ -1,10 +1,7 @@
 package com.sist.dao;
 
 public class test성민 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hi");
+		System.out.println();
 	}
-
 }
