708e1315de769aedc04053a68b23837742fa3822
USER <USER@DESKTOP-K2044LU>
2023-01-18 17:30:40 +0900

    MainModel 생성

---
+++ SeoulProject_4/src/main/java/com/sist/model/MainModel.java
@@ -0,0 +1,22 @@
+package com.sist.model;
+
+import com.sist.controller.Controller;
+import com.sist.controller.RequestMapping;
+
+import java.util.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sist.dao.*;
+import com.sist.vo.*;
+@Controller
+public class MainModel {
+	@RequestMapping("main/main.do")
+	public String main_page(HttpServletRequest request,HttpServletResponse response) {
+		
+		request.setAttribute("main_jsp", "../main/home.jsp");
+		return"../main/main.jsp";
+	}
+
+}
