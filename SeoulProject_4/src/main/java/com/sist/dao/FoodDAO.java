2e76930d4d7ea7794e2663dc70813cba0555d31e
mechuuri <pake7@naver.com>
2023-01-20 22:08:01 +0900

    230120_맛집중간과정

--- SeoulProject_4/src/main/java/com/sist/dao/FoodDAO.java
+++ SeoulProject_4/src/main/java/com/sist/dao/FoodDAO.java
@@ -1,27 +1,22 @@
 package com.sist.dao;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
-
+import java.util.*;
+import java.sql.*;
 import com.sist.vo.*;
 
 public class FoodDAO {
 	private Connection conn;
 	private PreparedStatement ps;
 	
-	//메인페이지 맛집리스트 : 실제 작업 시 이름 수정 (컬럼, 테이블 등)
+	// 카테고리 데이터
 	public ArrayList<FoodCategoryVO> mainpageFoodListData()
 	{
-		ArrayList<FoodCategoryVO> list=new ArrayList<FoodCategoryVO>(); 
+		ArrayList<FoodCategoryVO> list=new ArrayList<FoodCategoryVO>();
 		try
 		{
-			
 			conn=CreateConnection.getConnection();
-			String sql="SELECT /*+ INDEX_ASC(gg_foodcategory_4 fc_fcno_pk_4)*/ "
-					   +"fcno,title,subtitle,image "
-					   +"FROM gg_foodcategory_4 ";
+			String sql="SELECT /*+ INDEX_ASC(gg_foodCategory_4 fc_fcno_pk_4)*/ "
+					  +"fcno,title,subtitle,image "
+					  +"FROM gg_foodCategory_4";
 			ps=conn.prepareStatement(sql);
 			ResultSet rs=ps.executeQuery();
 			while(rs.next())
@@ -31,6 +26,54 @@ public class FoodDAO {
 				vo.setTitle(rs.getString(2));
 				vo.setSubtitle(rs.getString(3));
 				vo.setImage(rs.getString(4));
+				list.add(vo);
+				
+			}
+			rs.close();
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		finally
+		{
+			CreateConnection.disConnection(conn, ps);
+		}
+		return list;
+	}
+	// 검색 검색 검색 검색 검색 검색 (서울 맛집 ㅋ)
+	public ArrayList<FoodVO> foodLocationFindData(int page, String ss)
+	{
+		ArrayList<FoodVO> list=new ArrayList<FoodVO>();
+		try
+		{
+			conn=CreateConnection.getConnection();
+			String sql="SELECT fno,name,poster,num "
+					+"FROM (SELECT fno,name,poster,rownum as num "
+					+"FROM (SELECT fno,name,poster "
+					+"FROM gg_locationFood_4 "
+					+"WHERE address LIKE '%'||?||'%')) "
+					+"WHERE num BETWEEN ? AND ?";
+			
+			ps=conn.prepareStatement(sql);
+			int rowSize=20;
+			int start=(rowSize*page)-(rowSize-1);
+			int end=rowSize*page;
+			
+			ps.setString(1, ss);
+			ps.setInt(2, start);
+			ps.setInt(3, end);
+			
+			ResultSet rs=ps.executeQuery();
+			while(rs.next())
+			{
+				FoodVO vo=new FoodVO();
+				vo.setFno(rs.getInt(1));
+				vo.setName(rs.getString(2));
+				String poster=rs.getString(3);
+				poster=poster.substring(0,poster.indexOf("^"));
+				poster=poster.replace("#", "&");
+				vo.setPoster(poster);
+				
 				list.add(vo);
 			}
 			rs.close();
@@ -43,5 +86,103 @@ public class FoodDAO {
 			CreateConnection.disConnection(conn, ps);
 		}
 		return list;
+		
+	}
+	
+	public int foodLocationTotalPage(String ss)
+	{
+		int total=0;
+		try
+		{
+			conn=CreateConnection.getConnection();
+			String sql="SELECT CEIL(COUNT(*)/20.0) FROM gg_locationFood_4 "
+					+"WHERE address Like '%'||?||'$'";
+			ps=conn.prepareStatement(sql);
+			ps.setString(1, ss);
+			
+			ResultSet rs=ps.executeQuery();
+			
+			rs.next();
+			total=rs.getInt(1);
+			rs.close();
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		finally
+		{
+			CreateConnection.disConnection(conn, ps);
+		}
+		return total;
+	}
+	// 목록
+	public List<FoodVO> foodListData(int cno)
+	{
+		
+		List<FoodVO> list=new ArrayList<FoodVO>();
+		try
+		{
+			conn=CreateConnection.getConnection();
+			String sql="SELECT fno,fcno,name,addr,tel,type,poster,score "
+					  +"FROM gg_foodDetail_4 "
+					  +"WHERE fcno=?";
+			ps=conn.prepareStatement(sql);
+			ps.setInt(1, cno);
+			ResultSet rs=ps.executeQuery();
+			while(rs.next())
+			{
+				FoodVO vo=new FoodVO();
+				vo.setFno(rs.getInt(1));
+				vo.setFcno(rs.getInt(2));
+				vo.setName(rs.getString(3));
+				String addr=rs.getString(4);
+				addr=addr.substring(0,addr.lastIndexOf("지"));
+				vo.setAddr(addr.trim());
+				vo.setTel(rs.getString(5));
+				vo.setType(rs.getString(6));
+				String poster=rs.getString(7);
+				poster=poster.substring(0,poster.indexOf("^"));
+				vo.setPoster(poster);
+				vo.setScore(rs.getDouble(8));
+				list.add(vo);
+				
+			}
+			rs.close();
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		finally
+		{
+			CreateConnection.disConnection(conn, ps);
+		}
+		return list;
+	}
+	// 카테고리 제목 부제목
+	public FoodCategoryVO categoryInfoData(int cno)
+	{
+		FoodCategoryVO vo=new FoodCategoryVO();
+		try
+		{
+			conn=CreateConnection.getConnection();
+			String sql="SELECT title,subtitle FROM gg_foodCategory_4 "
+					  +"WHERE fcno=?";
+			ps=conn.prepareStatement(sql);
+			ps.setInt(1, cno);
+			ResultSet rs=ps.executeQuery();
+			rs.next();
+			vo.setTitle(rs.getString(1));
+			vo.setSubtitle(rs.getString(2));
+			rs.close();
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		finally
+		{
+			CreateConnection.disConnection(conn, ps);
+		}
+		return vo;
+				
 	}
 }
--- SeoulProject_4/src/main/java/com/sist/model/FoodModel.java
+++ SeoulProject_4/src/main/java/com/sist/model/FoodModel.java
@@ -1,5 +1,47 @@
 package com.sist.model;
 
+import com.sist.controller.Controller;
+import com.sist.controller.RequestMapping;
+
+import java.util.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.sql.*;
+import com.sist.vo.*;
+import com.sist.dao.*;
+@Controller
 public class FoodModel {
-//
+	@RequestMapping("food/food_location.do")
+	public String food_location(HttpServletRequest request,HttpServletResponse response)
+	{
+		try
+		{
+			request.setCharacterEncoding("UTF-8");
+		}catch(Exception ex) {}
+		String ss=request.getParameter("ss");
+		if(ss==null)
+			ss="마곡";
+		
+		String page=request.getParameter("page");
+		if(page==null)
+			page="1";
+		
+		// 현재페이지
+		int curpage=Integer.parseInt(page);
+		// dao에서 데이터 읽기
+		FoodDAO dao=new FoodDAO();
+		ArrayList<FoodVO> list=dao.foodLocationFindData(curpage, ss);
+		int totalpage=dao.foodLocationTotalPage(ss);
+		
+		request.setAttribute("list", list);
+		request.setAttribute("curpage", curpage);
+		request.setAttribute("totalpage", totalpage);
+		request.setAttribute("ss", ss);
+		request.setAttribute("main_jsp", "../food/food_location.jsp");
+		return "../main/main.jsp";
+				
+	}
+	
 }
