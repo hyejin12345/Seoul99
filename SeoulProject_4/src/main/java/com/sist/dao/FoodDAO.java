d817fe16ad03b60b57dc7b202b592ada1c45b9e3
mechuuri <pake7@naver.com>
2023-01-25 01:35:54 +0900

    230125_foodDAO수정,foodModel수정,food_location.jsp추가(배열오류,css추후),food_list.jsp(~추후)

--- SeoulProject_4/src/main/java/com/sist/dao/FoodDAO.java
+++ SeoulProject_4/src/main/java/com/sist/dao/FoodDAO.java
@@ -40,7 +40,7 @@ public class FoodDAO {
 		}
 		return list;
 	}
-	// 검색 검색 검색 검색 검색 검색 (서울 맛집 ㅋ)
+	// 검색 
 	public ArrayList<FoodVO> foodLocationFindData(int page, String ss)
 	{
 		ArrayList<FoodVO> list=new ArrayList<FoodVO>();
@@ -51,14 +51,13 @@ public class FoodDAO {
 					+"FROM (SELECT fno,name,poster,rownum as num "
 					+"FROM (SELECT fno,name,poster "
 					+"FROM gg_locationFood_4 "
-					+"WHERE address LIKE '%'||?||'%')) "
-					+"WHERE num BETWEEN ? AND ?";
+				    +"WHERE addr LIKE '%'||?||'%')) "
+				    +"WHERE num BETWEEN ? AND ?";
 			
 			ps=conn.prepareStatement(sql);
 			int rowSize=20;
 			int start=(rowSize*page)-(rowSize-1);
 			int end=rowSize*page;
-			
 			ps.setString(1, ss);
 			ps.setInt(2, start);
 			ps.setInt(3, end);
@@ -91,19 +90,19 @@ public class FoodDAO {
 	
 	public int foodLocationTotalPage(String ss)
 	{
-		int total=0;
+		int totalpage=0;
 		try
 		{
 			conn=CreateConnection.getConnection();
 			String sql="SELECT CEIL(COUNT(*)/20.0) FROM gg_locationFood_4 "
-					+"WHERE address Like '%'||?||'$'";
+					+"WHERE addr LIKE '%'||?||'%'";
 			ps=conn.prepareStatement(sql);
 			ps.setString(1, ss);
 			
 			ResultSet rs=ps.executeQuery();
 			
 			rs.next();
-			total=rs.getInt(1);
+			totalpage=rs.getInt(1);
 			rs.close();
 		}catch(Exception ex)
 		{
@@ -113,37 +112,36 @@ public class FoodDAO {
 		{
 			CreateConnection.disConnection(conn, ps);
 		}
-		return total;
+		return totalpage;
 	}
 	// 목록
-	public List<FoodVO> foodListData(int cno)
+	public ArrayList<FoodVO> foodListData(int fcno)
 	{
 		
-		List<FoodVO> list=new ArrayList<FoodVO>();
+		ArrayList<FoodVO> list=new ArrayList<FoodVO>();
 		try
 		{
 			conn=CreateConnection.getConnection();
-			String sql="SELECT fno,fcno,name,addr,tel,type,poster,score "
+			String sql="SELECT /*+ INDEX_ASC(gg_foodDetail_4 fd_fno_pk)*/fno,name,addr,tel,type,poster,score "
 					  +"FROM gg_foodDetail_4 "
 					  +"WHERE fcno=?";
 			ps=conn.prepareStatement(sql);
-			ps.setInt(1, cno);
+			ps.setInt(1, fcno);
 			ResultSet rs=ps.executeQuery();
 			while(rs.next())
 			{
 				FoodVO vo=new FoodVO();
 				vo.setFno(rs.getInt(1));
-				vo.setFcno(rs.getInt(2));
-				vo.setName(rs.getString(3));
-				String addr=rs.getString(4);
+				vo.setName(rs.getString(2));
+				String addr=rs.getString(3);
 				addr=addr.substring(0,addr.lastIndexOf("지"));
 				vo.setAddr(addr.trim());
-				vo.setTel(rs.getString(5));
-				vo.setType(rs.getString(6));
-				String poster=rs.getString(7);
+				vo.setTel(rs.getString(4));
+				vo.setType(rs.getString(5));
+				String poster=rs.getString(6);
 				poster=poster.substring(0,poster.indexOf("^"));
 				vo.setPoster(poster);
-				vo.setScore(rs.getDouble(8));
+				vo.setScore(rs.getDouble(7));
 				list.add(vo);
 				
 			}
@@ -158,8 +156,8 @@ public class FoodDAO {
 		}
 		return list;
 	}
-	// 카테고리 제목 부제목
-	public FoodCategoryVO categoryInfoData(int cno)
+	// 카테고리 정보
+	public FoodCategoryVO categoryInfoData(int fcno)
 	{
 		FoodCategoryVO vo=new FoodCategoryVO();
 		try
@@ -168,7 +166,7 @@ public class FoodDAO {
 			String sql="SELECT title,subtitle FROM gg_foodCategory_4 "
 					  +"WHERE fcno=?";
 			ps=conn.prepareStatement(sql);
-			ps.setInt(1, cno);
+			ps.setInt(1, fcno);
 			ResultSet rs=ps.executeQuery();
 			rs.next();
 			vo.setTitle(rs.getString(1));
@@ -183,6 +181,53 @@ public class FoodDAO {
 			CreateConnection.disConnection(conn, ps);
 		}
 		return vo;
-				
+	}
+	// 맛집 상세보기 출력 
+	public FoodVO foodDetail(int fno)
+	{
+		FoodVO vo=new FoodVO();
+		try
+		{
+			conn=CreateConnection.getConnection();
+			String sql="UPDATE gg_foodDetail_4 SET "
+					  +"hit=hit=1 "
+					  +"WHERE fno=?";
+			ps=conn.prepareStatement(sql);
+			ps.setInt(1, fno);
+			ps.executeUpdate();
+			
+			sql="SELECT fno,name,tel,score,poster,addr,type,time,parking,menu,price,good,soso,bad "
+			   +"FROM gg_foodDetail_4 "
+			   +"WHERE fno=?";
+			ps=conn.prepareStatement(sql);
+			ps.setInt(1, fno);
+			ResultSet rs=ps.executeQuery();
+			rs.next();
+			vo.setFno(rs.getInt(1));
+			vo.setName(rs.getString(2));
+			vo.setTel(rs.getString(3));
+			vo.setScore(rs.getDouble(4));
+			vo.setPoster(rs.getString(5));
+			vo.setAddr(rs.getString(6));
+			vo.setType(rs.getString(7));
+			vo.setTime(rs.getString(8));
+			vo.setParking(rs.getString(9));
+			vo.setMenu(rs.getString(10));
+			vo.setPrice(rs.getString(11));
+			vo.setGood(rs.getInt(12));
+			vo.setSoso(rs.getInt(13));
+			vo.setBad(rs.getInt(14));
+			rs.close();
+			
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		finally
+		{
+			CreateConnection.disConnection(conn, ps);
+		}
+		return vo;
+		
 	}
 }
